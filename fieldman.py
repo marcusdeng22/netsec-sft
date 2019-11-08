0adfd7141705db8c8fddbf9d40e08e3a442c81a7
BradROlbrey <berdley1@gmail.com>
2019-11-08 12:29:39 -0600

    Messing around with string formatting and str-to-int conversions

--- fieldman.py
+++ fieldman.py
@@ -1,9 +1,26 @@
 
 import socket
+import hashlib
+import binascii
 
 HOST = 'localhost'
 PORT = 45678
 
+SECRET_KEY = '0123456789abcdef'
+print(SECRET_KEY)
+
+bitstring = ''
+for c in SECRET_KEY:
+    bitstring += '{0:04b}'.format(int(c, 16))
+print(bitstring, len(bitstring))
+
+print(int(SECRET_KEY, 16))
+print(int(bitstring, 2))
+
+SECRET_KEY = int(SECRET_KEY, 16)
+print(SECRET_KEY ^ SECRET_KEY)
+
+'''
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
@@ -16,3 +33,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             break
 
         s.sendall(bytes(to_send, 'utf-8'))
+'''
