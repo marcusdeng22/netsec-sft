a5d9be2f0eced06312b7eeb2a14fb32d84d9e794
BradROlbrey <berdley1@gmail.com>
2019-11-08 15:19:05 -0600

    A single 64-bit block of key and text nicely printed to the screen

--- fieldman.py
+++ fieldman.py
@@ -12,25 +12,59 @@ print(SECRET_KEY)
 bitstring = ''
 for c in SECRET_KEY:
     bitstring += '{0:04b}'.format(int(c, 16))
-print(bitstring, len(bitstring))
+#print(bitstring)
 
-print(int(SECRET_KEY, 16))
-print(int(bitstring, 2))
+for i in range(0, len(bitstring), 8):
+    print(bitstring[i:i+8], end=' ')
+print(len(bitstring))
+
+#print(int(SECRET_KEY, 16))
+#print(int(bitstring, 2))
 
 SECRET_KEY = int(SECRET_KEY, 16)
-print(SECRET_KEY ^ SECRET_KEY)
+#print(SECRET_KEY ^ SECRET_KEY)
 
-'''
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+length_to_print = 8
 
-    while True:  # Using break statement to emulate do-while loop.
+SECRET_MESSAGE = 'The man in Black fled across the Desert, and the Gunslinger followed.'
+for c in SECRET_MESSAGE[:length_to_print]:
+    print('{0:>8}'.format(c), end=' ')
+print()
 
-        to_send = input(":> ")
-        if to_send == 'quit' or \
-            to_send == 'exit' or \
-            to_send == 'q':
-            break
 
-        s.sendall(bytes(to_send, 'utf-8'))
-'''
+# For some reason this doesn't work?
+# SECRET_NUMBERS1 = binascii.hexlify(SECRET_MESSAGE.encode('utf-8'))
+# print()
+# for i in SECRET_NUMBERS1[:10]:
+#     print('{0:>3}'.format(i), end=' ')
+
+SECRET_NUMBERS2 = [binascii.hexlify(c.encode('utf-8')) for c in SECRET_MESSAGE]
+
+for i in SECRET_NUMBERS2[:length_to_print]:
+    print('{0:8x}'.format(int(i, 16)), end=' ')
+print()
+
+for i in SECRET_NUMBERS2[:length_to_print]:
+    print('{0:8}'.format(int(i, 16)), end=' ')
+print()
+
+for i in SECRET_NUMBERS2[:length_to_print]:
+    print('{0:08b}'.format(int(i, 16)), end=' ')
+print('\n')
+
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+
+#     # Iterate over the message block by block in here.
+#     #for i in range
+
+#     s.sendall(bytes(SECRET_MESSAGE, 'utf-8'))
+
+#     # while True:  # Using break statement to emulate do-while loop.
+#         # to_send = input(":> ")
+#         # if to_send == 'quit' or \
+#         #     to_send == 'exit' or \
+#         #     to_send == 'q':
+#         #     break
+#         # s.sendall(bytes(to_send, 'utf-8'))
