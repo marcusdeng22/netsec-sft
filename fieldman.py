ff78ec90f6faa7e5e78228c9c8d5ac5af1a0b186
BradROlbrey <berdley1@gmail.com>
2019-11-12 12:45:28 -0600

    One encrypted block sent from client to server
    Client sends a single encrypted block to the server.
    Server receives encrypted block and decrypts it, printing it to screen.

--- fieldman.py
+++ fieldman.py
@@ -5,7 +5,14 @@ import socket
 
 
 def main():
-    block_size_bytes = 16
+    SECRET_KEY = '0123456789abcdef'
+        # Must be of even length. Each character only creates 4 bits of an integer,
+        # and reading a file creates a bytes object, where each byte element
+        # is 8 bits (obviously).
+    block_size_bytes = len(SECRET_KEY) // 2
+    SECRET_KEY = int(SECRET_KEY, 16)
+    print('{0:0>{1}b}'.format(SECRET_KEY, block_size_bytes*8))
+    print()
 
     HOST = 'localhost'
     PORT = 45678
@@ -13,12 +20,14 @@ def main():
         s.connect((HOST, PORT))
 
         with open("input.txt", 'rb') as file:
+        #with open("pitfalls.pptx", 'rb') as file:
                 
             while True:
-                # Read a block_size bytes from the file.
+                # Read block_size bytes from the file.
                 from_file = file.read(block_size_bytes)
                     # Result is a bytes-like object of x bytes, though each element
                     # is an int already.
+                print("from_file:", from_file)
                 
 
                 # If end of file,
@@ -30,10 +39,34 @@ def main():
 
                     break
                 
-                # else, XOR bytes with key and send block_size bytes of data
-                # at a time.
-                s.sendall(from_file)
-                print('\"', from_file.decode('utf-8'), '\"', sep='')
+                # else, process a block of bytes at a time.
+
+                # Convert it into a proper integer bitstring.
+                unencrypted_int = 0
+                for idx, byte in enumerate(from_file):
+                    unencrypted_int = (unencrypted_int << 8) | byte
+                    print('{a:0{b}b} {c:08b} {d}'.format(a=unencrypted_int, b=idx*8, c=byte, d=byte))
+                
+                print('\nUnencrypted integer: {0:0>{1}b}'.format(unencrypted_int, block_size_bytes*8))
+
+                # Encrypt it by XOR'ing it with the key.
+                encrypted_int = unencrypted_int ^ SECRET_KEY
+                print('Encrypted integer: {0:0>{1}b}'.format(encrypted_int, block_size_bytes*8))
+
+                # Convert it back to a string object (so we can encode it).
+                # Each bit is a character.
+                encrypted_bitstring = '{0:0>{1}b}'.format(encrypted_int, block_size_bytes*8)
+                print('Encrypted bitstring:', encrypted_bitstring)
+
+                # Convert to bytes (to send over network).
+                # Each bit is a byte.
+                encrypted_bytes = encrypted_bitstring.encode('utf-8')
+                print("Encrypted bytes:", encrypted_bytes)
+
+                # Send over network.
+                s.sendall(encrypted_bytes)
+
+                break
 
 
 if __name__ == '__main__':
--- guyinthechair.py
+++ guyinthechair.py
@@ -5,56 +5,90 @@ import socket
 
 
 def main():
+    SECRET_KEY = '0123456789abcdef'
+    block_size_bytes = len(SECRET_KEY) // 2
+    SECRET_KEY = int(SECRET_KEY, 16)
+    print('{0:0>{1}b}'.format(SECRET_KEY, block_size_bytes*8))
+
     HOST = 'localhost'
     PORT = 45678
-    block_size_bytes = 16
-
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()  # Sets the server to listen for incoming requests
 
-        while True:
-            print("\nListening for incoming connections")
-            conn, addr = s.accept()  # Block here until request received
-            # Accepts creates a new socket object that we'll be using to
-            # communicate with the client.
-            # It's different from the socket the server is using to listen
-            # for new connections, as we can see with the different port.
+        print("\nListening for incoming connections\n")
+        conn, addr = s.accept()  # Block here until request received
+        # Accepts creates a new socket object that we'll be using to
+        # communicate with the client.
+        # It's different from the socket the server is using to listen
+        # for new connections, as we can see with the different port.
 
-            with conn:
-                print("Connection from", addr)
-                
-                # maintain two vars, 'buff' and 'incoming'.
-                #   recv'ing into 'incoming' instead of immediately appending to
-                #       'buff' allows us to check for empty string.
-                #   appending to 'buff' instead of immediately recv'ing to 'buff'
-                #       allows us to maintain any bytes leftover from while loop
-                #       when len(buff) < block_size.
-
-                buff = ''.encode('utf-8')
-                # Initialized outside of loop so we can
-                #   1. append to it inside of the loop,
-                #   2. deal with last less-than-block_size bytes of data after loop.
-                
-                while True:
-                    incoming = conn.recv(1024)
-
-                    # if we recv'd empty string, client closed connection,
-                    if not incoming:
-                        break  # so break from loop.
-                        # buff will be less than block_size bytes b/c while loop.
-                    
-                    # else, continue processing data
-                    buff += incoming
-
-                    # Want at least block_size bytes to XOR with our key.
-                    while len(buff) >= block_size_bytes:
-                        work_with, buff = buff[:block_size_bytes], buff[block_size_bytes:]
-                        print('\"', work_with.decode('utf-8'), '\"', sep='')
-                    
+        with conn:
+            #print("Connection from", addr)
             
+            # maintain two vars, 'buff' and 'incoming'.
+            #   recv'ing into 'incoming' instead of immediately appending to
+            #       'buff' allows us to check for empty string.
+            #   appending to 'buff' instead of immediately recv'ing to 'buff'
+            #       allows us to maintain any bytes leftover from while loop
+            #       when len(buff) < block_size.
+
+            buff = ''.encode('utf-8')
+            # Initialized outside of loop so we can
+            #   1. append to it inside of the loop,
+            #   2. deal with last less-than-block_size bytes of data after loop.
+            
+            while True:
+                incoming = conn.recv(1024)
+                #print(type(incoming))
+
+                # if we recv'd empty string, client closed connection,
+                if not incoming:
+                    break  # so break from loop.
+                    # buff will be less than block_size bytes b/c while loop.
+                
+                # else, continue processing data
+
+                buff += incoming
+
+                # Want at least block_size bytes to XOR with our key.
+                while len(buff) >= block_size_bytes:
+                    # Receive a block of encrypted bytes. Each byte is one bit of the bitstring.
+                    encrypted_bytes, buff = buff[:block_size_bytes*8], buff[block_size_bytes*8:]
+                    print('Encrypted bytes:', encrypted_bytes)
+
+                    # Decode the encrypted bytes to get a bitstring. Each character is one bit.
+                    encrypted_bitstring = encrypted_bytes.decode('utf-8')
+                    print('Encrypted bitstring:', encrypted_bitstring)
+                    
+                    # Convert the bitstring into an integer
+                    encrypted_int = int(encrypted_bitstring, 2)
+                    print('Encrypted integer: {0:0>{1}b}'.format(encrypted_int, block_size_bytes*8))
+
+                    # Decrypt it by XOR'ing it with the key.
+                    decrypted_int = encrypted_int ^ SECRET_KEY
+                    print('Decrypted integer: {0:0>{1}b}'.format(decrypted_int, block_size_bytes*8))
+
+                    # Break down the integer into a list of individual ints (so we can
+                    # convert it back into a bytes object).
+                    print()
+                    int_list = []
+                    for i in range(block_size_bytes):
+                        # Take the 8 LSB and insert it at front of list
+                        lsb = 0b11111111 & decrypted_int
+                        int_list.insert(0, lsb)
+                        print('{a:0{b}b} {c:08b} {d}'.format(a=decrypted_int, b=(block_size_bytes*8)-(i*8), c=lsb, d=int_list[0]))
+                        decrypted_int >>= 8  # Until decrypted_int is no more
+
+                    # Convert list_int into a bytes object.
+                    to_file = bytes(int_list)
+                    print("to_file:", to_file)
+
+                    break
+                break
+                
                 # Deal with the last less-than-block_size bytes of data
-                print('\"', buff.decode('utf-8'), '\"\nfinished.', sep='')
+                #print('\"', buff.decode('utf-8'), '\"\nfinished.', sep='')
                 
 
 if __name__ == '__main__':
