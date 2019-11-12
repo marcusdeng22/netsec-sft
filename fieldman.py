d2c6ff241e1050d6c12ab86446dc462bdd2f8705
BradROlbrey <berdley1@gmail.com>
2019-11-12 11:36:52 -0600

    Finally figured out what I'm doing
    Client processes input file block_size bytes at a time and sends to server.
    Server processes received data block_size bytes at a time, prints to console.

--- fieldman.py
+++ fieldman.py
@@ -1,22 +1,11 @@
 
-import sys
+# Client
+
 import socket
-import hashlib
-import binascii
-
-
-length_to_print = 8
 
 
 def main():
-    # Make a dumb secret key
-    SECRET_KEY = '0123456789abcdef'
-    block_size_bytes = len(SECRET_KEY)
-    SECRET_KEY = [binascii.hexlify(c.encode('utf-8')) for c in SECRET_KEY]
-    SECRET_KEY = bit_stringify(SECRET_KEY)
-    print(SECRET_KEY)
-
-
+    block_size_bytes = 16
 
     HOST = 'localhost'
     PORT = 45678
@@ -26,88 +15,25 @@ def main():
         with open("input.txt", 'rb') as file:
                 
             while True:
-                # Read a block of bytes from the file.
+                # Read a block_size bytes from the file.
                 from_file = file.read(block_size_bytes)
                     # Result is a bytes-like object of x bytes, though each element
                     # is an int already.
+                
 
+                # If end of file,
                 if len(from_file) < block_size_bytes:
                     s.sendall(from_file)
-                    break
                     # Only XOR as much of the key as there is message.
                     # Shift left or right?
-                    
+                    print('\"', from_file.decode('utf-8'), '\"\nfinished.', sep='')
 
+                    break
+                
+                # else, XOR bytes with key and send block_size bytes of data
+                # at a time.
                 s.sendall(from_file)
-
-
-
-
-
-    # secret_message = 'The man in Black fled across the Desert, and the Gunslinger followed.'
-    # for c in secret_message[:length_to_print]:
-    #     print('{0:>8}'.format(c), end=' ')
-    # print()
-
-    # secret_nums = bit_stringify(secret_message)
-
-    # # We have a secret block, finally.
-    # print('secret:    {0:0>64b}'.format(secret_nums))
-    # print('key:       {0:0>64b}'.format(SECRET_KEY))
-    # secret_block = secret_nums ^ SECRET_KEY
-    # print('ecnrypted: {0:0>64b}'.format(secret_block))
-
-
-
-
-
-# Takes the entire message as a string.
-# Returns the first eight characters as a number.
-#   I wonder if encoding the whole message will produce issues with the
-#   number becoming too big? Might be better practice to iterate x bytes
-#   at a time instead of the whole message.
-def bit_stringify(inp_str):
-
-    # # Convert the string to hex
-    # secret_chars = [binascii.hexlify(c.encode('utf-8')) for c in inp_str]
-
-    # # Check out some print formattings
-    # for i in secret_chars[:length_to_print]:
-    #     print('{0:8x}'.format(int(i, 16)), end=' ')
-    # print()
-
-    # for i in secret_chars[:length_to_print]:
-    #     print('{0:8}'.format(int(i, 16)), end=' ')
-    # print()
-
-    # for i in secret_chars[:length_to_print]:
-    #     print('{0:08b}'.format(int(i, 16)), end=' ')
-    # print('\n')
-
-    secret_chars = inp_str
-
-    # Combine the elements into a 64 bit bitstring 8 bits at a time
-    #   and combine them.
-    # Can't ''.join the elements of the SECRET_CHARS list because they
-    #   are all encoded and something goes wrong decoding them blah blah.
-    bitstring = ''
-    for c in secret_chars[:8]:
-        bitstring += '{0:08b}'.format(int(c, 16))
-    #print(bitstring)
-
-    # for i in range(0, len(bitstring), 8):
-    #     print(SECRET_NUMS[i:i+8], end=' ')
-    # print(len(SECRET_NUMS))
-
-
-    # Convert the message into an integer.
-    secret_nums = int(bitstring, 2)
-    print(secret_nums)
-    print(bitstring)
-    print('{0:0>64b}'.format(secret_nums))
-    print()
-
-    return secret_nums
+                print('\"', from_file.decode('utf-8'), '\"', sep='')
 
 
 if __name__ == '__main__':
--- guyinthechair.py
+++ guyinthechair.py
@@ -1,29 +1,62 @@
 
 # Server
 
-print("hello there, fool!")
-
 import socket
 
-HOST = 'localhost'
-PORT = 45678
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()  # Sets the server to listen for incoming requests
+def main():
+    HOST = 'localhost'
+    PORT = 45678
+    block_size_bytes = 16
 
-    while True:
-        print("\nListening for incoming connections")
-        conn, addr = s.accept()  # Block here until request received
-        # Accepts creates a new socket object that we'll be using to
-        # communicate with the client.
-        # It's different from the socket the server is using to listen
-        # for new connections, as we can see with the different port.
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()  # Sets the server to listen for incoming requests
 
-        with conn:
-            print("Connection from", addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(data.decode('utf-8'))
+        while True:
+            print("\nListening for incoming connections")
+            conn, addr = s.accept()  # Block here until request received
+            # Accepts creates a new socket object that we'll be using to
+            # communicate with the client.
+            # It's different from the socket the server is using to listen
+            # for new connections, as we can see with the different port.
+
+            with conn:
+                print("Connection from", addr)
+                
+                # maintain two vars, 'buff' and 'incoming'.
+                #   recv'ing into 'incoming' instead of immediately appending to
+                #       'buff' allows us to check for empty string.
+                #   appending to 'buff' instead of immediately recv'ing to 'buff'
+                #       allows us to maintain any bytes leftover from while loop
+                #       when len(buff) < block_size.
+
+                buff = ''.encode('utf-8')
+                # Initialized outside of loop so we can
+                #   1. append to it inside of the loop,
+                #   2. deal with last less-than-block_size bytes of data after loop.
+                
+                while True:
+                    incoming = conn.recv(1024)
+
+                    # if we recv'd empty string, client closed connection,
+                    if not incoming:
+                        break  # so break from loop.
+                        # buff will be less than block_size bytes b/c while loop.
+                    
+                    # else, continue processing data
+                    buff += incoming
+
+                    # Want at least block_size bytes to XOR with our key.
+                    while len(buff) >= block_size_bytes:
+                        work_with, buff = buff[:block_size_bytes], buff[block_size_bytes:]
+                        print('\"', work_with.decode('utf-8'), '\"', sep='')
+                    
+            
+                # Deal with the last less-than-block_size bytes of data
+                print('\"', buff.decode('utf-8'), '\"\nfinished.', sep='')
+                
+
+if __name__ == '__main__':
+    print("hello there, fool!")
+    main()
